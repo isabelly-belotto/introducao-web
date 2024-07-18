dd01ca941a8dcf1a4cfb04776d78703c5c69b667
isabelly-belotto <isabellybelotto.vitoria@gmail.com>
2024-07-17 22:03:14 -0300

    Initial commit

---
+++ java.js
@@ -0,0 +1,18 @@
+
+
+function addNewTask() {
+  var list = document.getElementById("list");
+  var text = document.getElementById("task_name").value;
+
+  if (text.length === 0) {
+    alert("Tarefa precisa de ter mais de 1 caracter");
+    return;
+  }
+  
+  var listitem = document.createElement("li");
+  listitem.className = "list-item";
+
+  const textElement = document.createTextNode(text);
+  listitem.appendChild(textElement);
+  list.appendChild(listitem);
+}
